8cea9be4283974470caafa40ab4099f88399bd71
iyasalowaneh <iyas_moh@yahoo.com>
2021-05-19 09:28:11 +0300

    Done

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -9,8 +9,10 @@
  * isArrayLengthOdd([1, 2, 3, 4]) -> flase
  */
 function isArrayLengthOdd(numbers) {
-  // Your code here
+  if (numbers.length % 2 === 0) return false;
+  else return true;
 }
+// Your code here
 
 /**
  * isArrayLengthEven(numbers):
@@ -23,7 +25,8 @@ function isArrayLengthOdd(numbers) {
  * isArrayLengthEven([1, 2, 3, 4]) -> true
  */
 function isArrayLengthEven(numbers) {
-  // Your code here
+  if (numbers.length % 2 === 0) return true;
+  else return false;
 }
 
 /**
@@ -35,6 +38,8 @@ function isArrayLengthEven(numbers) {
  * addLailaToArray(["Mshary", "Hasan"]) -> ["Mshary", "Hasan", "Laila"]
  */
 function addLailaToArray(instructors) {
+  instructors.push("Laila");
+  return instructors;
   // Your code here
 }
 
@@ -47,7 +52,8 @@ function addLailaToArray(instructors) {
  * eliminateTeam(["Brazil", "Germany", "Italy"]) -> "Italy"
  */
 function eliminateTeam(teams) {
-  // Your code here
+  let x = teams.pop();
+  return x;
 }
 
 /**
@@ -61,7 +67,8 @@ function eliminateTeam(teams) {
  * secondHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi", "blueberry"]) -> []
  */
 function secondHalfOfArrayIfItIsEven(fruits) {
-  // Your code here
+  if (fruits.length % 2 === 0) return ["banana", "kiwi"];
+  else return [];
 }
 
 /**
@@ -79,7 +86,11 @@ function secondHalfOfArrayIfItIsEven(fruits) {
  * - Use string method .endsWith()
  */
 function youGottaCalmDown(shout) {
-  // Your code here
+  while (shout.endsWith("!!")) {
+    shout = shout.slice(0, -1);
+  }
+
+  return shout;
 }
 
 module.exports = {
